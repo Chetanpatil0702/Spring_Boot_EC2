d472f3c471ecc334963878112b722b2ad3f18ba6
Pratik-Patil-7 <108pratikmpatil@gmail.com>
2026-01-06 14:16:49 +0530

    Update DemoController.java
    This is commit msg

--- src/main/java/com/example/Spring_Boot_EC2/Controller/DemoController.java
+++ src/main/java/com/example/Spring_Boot_EC2/Controller/DemoController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class DemoController {
 
-    // added comment
+    // Commnet
     @GetMapping()
     ResponseEntity<String> msg(){
        return ResponseEntity.ok("Hii Work Properly...!");
